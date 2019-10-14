44e59c50e786e0b2ba252daa5abf8be04023292b
Thilo Ilg <contact@thiloilg.com>
2019-10-14 20:30:20 +0200

    feat(ma): add simple code snippets

---
+++ snippets/examples/script.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+## install docker
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
+
+## install docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# create docker network
+docker network create web
