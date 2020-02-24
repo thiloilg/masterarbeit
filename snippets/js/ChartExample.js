41eead9a1220a9772055f5828f3a8ce2f3679aaa
Thilo Ilg <contact@thiloilg.com>
2020-02-24 18:26:08 +0100

    feat(ma): add diagrammbeispiel

---
+++ snippets/js/ChartExample.js
@@ -0,0 +1,21 @@
+export function ChartExample(
+    data = [],
+    onDataUpdate = () => {},
+    settings = {},
+    setSettings = () => {}
+  ) {
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')
+  
+    const chart = new Chart(ctx, {
+        tye: 'line',
+        data,
+    })
+  
+    onDataUpdate(updatedData => {
+        chart.data = updatedData
+        chart.update()
+    })
+  
+    return canvas
+}
